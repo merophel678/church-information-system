24617b96af67ed139f46a06cf53792a83407bf6c
Kenn Henzly Ciar <ciarkenken12@gmail.com>
2025-12-07 14:55:33 +0800

    chore: add shims to unblock server build on Railway

--- server/src/types/shims.d.ts
+++ server/src/types/shims.d.ts
@@ -1,53 +1,34 @@
 declare module 'express' {
-  export interface Request {
-    [key: string]: any;
-  }
-
-  export interface Response {
-    status: (code: number) => Response;
-    json: (body: any) => Response;
-    send: (body?: any) => Response;
-  }
-
-  export type NextFunction = (err?: any) => void;
-
-  export interface RequestHandler {
-    (req: Request, res: Response, next?: NextFunction): any;
-  }
-
-  export interface Router {
-    use: (...handlers: RequestHandler[]) => Router;
-    use: (path: string, ...handlers: RequestHandler[]) => Router;
-    get: (path: string, ...handlers: RequestHandler[]) => Router;
-    post: (path: string, ...handlers: RequestHandler[]) => Router;
-    put: (path: string, ...handlers: RequestHandler[]) => Router;
-    delete: (path: string, ...handlers: RequestHandler[]) => Router;
-  }
-
-  export function Router(): Router;
-  const e: () => any;
+  const e: any;
   export default e;
+  export type Request = any;
+  export type Response = any;
+  export type NextFunction = any;
+  export type RequestHandler = any;
 }
 
 declare module 'cors' {
-  import { RequestHandler } from 'express';
-  function cors(options?: any): RequestHandler;
-  export default cors;
+  const c: any;
+  export default c;
 }
 
 declare module 'jsonwebtoken' {
-  export const sign: (...args: any[]) => string;
-  export const verify: (...args: any[]) => any;
+  const j: any;
+  export default j;
+  export const sign: any;
+  export const verify: any;
 }
 
 declare module 'bcrypt' {
-  export const hash: (...args: any[]) => Promise<string>;
-  export const compare: (...args: any[]) => Promise<boolean>;
+  const b: any;
+  export default b;
+  export const hash: any;
+  export const compare: any;
 }
 
 declare module 'multer' {
-  const multer: any;
-  export default multer;
+  const m: any;
+  export default m;
 }
 
 declare var process: any;
